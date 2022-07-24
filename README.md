bfbab1fa4ea23f3cbee90867d5a93b92f1d9648c
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2022-07-24 08:30:27 -0300

    add: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Maven Application Micronaut
+
+Aplicação em Java com Build em Maven e Framework Micronaut
---
+++ app/src/test/java/br/com/ervil/JavaMavenApplicationMicronautTest.java
@@ -0,0 +1,21 @@
+package br.com.ervil;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class JavaMavenApplicationMicronautTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
